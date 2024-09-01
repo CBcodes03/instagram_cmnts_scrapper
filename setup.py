76c3bd4a81c20fe3023cfeeb0c805fb1aa65e45d
chirag <chiragbudakoti12334@gmail.com>
2024-09-01 20:40:27 +0530

    to deploy

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import os
 
-def pre_download_deps():
+def build():
   os.system("sudo apt update")
   os.system("sudo apt install chromium-chromedriver chromium-browser")
   os.system("pip3 install selenium")
   os.system("pip3 install streamlit")
   return None
+
+build()
