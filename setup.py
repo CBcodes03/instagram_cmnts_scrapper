d9df37780f7940353de9a6aaff05a1df29142f49
chirag <chiragbudakoti12334@gmail.com>
2024-09-01 21:00:18 +0530

    final(hope)

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ def build():
   os.system("sudo apt install chromium-chromedriver chromium-browser")
   os.system("pip3 install selenium")
   os.system("pip3 install streamlit")
+  os.system("pip3 install webdriver-manager")
   return None
 
 build()
